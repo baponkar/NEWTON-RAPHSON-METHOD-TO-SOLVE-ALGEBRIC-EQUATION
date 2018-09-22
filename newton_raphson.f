2f4baed02573645fefbaa29be0c63951bf9c99fc
Bapon Kar <baponkar600@gmail.com>
2018-09-22 16:25:10 +0530

    Update newton_raphson.f

--- newton_raphson.f
+++ newton_raphson.f
@@ -1,38 +1,37 @@
-      !NEWTON RAPHSON METHOD
-      !TO SOLVE COSX=XEXP(X)
-      PROGRAM NWTN_RAPHSON
-       IMPLICIT NONE
-       REAL::X(0:100),F,TEMP=0,DEL_F
-       INTEGER::I,J,K
-        DO I=1,100
-          IF( F(TEMP)*F(TEMP+1) .LE. 0 )THEN
-          PRINT*,"THE ROOT LIES IN BETWEEN ",TEMP," AND",TEMP+1
-          EXIT
-          ENDIF
-          TEMP=TEMP+1
-        END DO
-         X(0)=TEMP
-         
-         DO I=0,100
-          X(I+1)=X(I)+ F(X(I))/DEL_F(X(I))
-          END DO
+    
+      !Find out the root of the equation exp(x) = 2x + 1 by Newton-Raphson method
+
+PROGRAM NEWTON_RAPHSON
+ IMPLICIT NONE
+
+ REAL::F,X(0:100),TEMP,del_f
+ INTEGER::I,J,K
+ 
+  PRINT*,"ENTER INITIAL VALUES OF X"
+   READ*,X(0)
+ 
+  DO I=0,99
+   print*,X(I)
+   X(I+1)=X(I)-F(X(I))/DEL_F(X(I))
+  
+  END DO
+
+   PRINT*,"THE REQUIRED ROOT IS:",X(100)
+
+END PROGRAM NEWTON_RAPHSON
+ 
 
 
-          PRINT*,"THE ROOT IS:",X(100)
-          !PAUSE
-       END PROGRAM NWTN_RAPHSON
-       
-       
-       REAL FUNCTION F(X)
-        IMPLICIT NONE
-        REAL,INTENT(IN)::X
-        F=X*COS(X)-X*EXP(X)
-        !PAUSE
-        END FUNCTION F
 
-        REAL FUNCTION DEL_F(X)
-        IMPLICIT NONE
-        REAL,INTENT(IN)::X
-        DEL_F=COS(X)-X*SIN(X)-EXP(X)-X*EXP(X)
-        PAUSE
-        END FUNCTION DEL_F
+REAL FUNCTION F(X)
+ IMPLICIT NONE
+ REAL,INTENT(IN)::X
+  F=EXP(X)-2*X-1
+END FUNCTION F
+
+
+REAL FUNCTION DEL_F(X)
+ IMPLICIT NONE
+ REAL,INTENT(IN)::X
+  DEL_F=EXP(X)-2
+END FUNCTION DEL_F
